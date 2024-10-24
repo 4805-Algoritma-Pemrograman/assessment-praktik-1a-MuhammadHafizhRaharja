892af878f63e28ce2c5af78e25e5b379a012cba2
Hafizh <110800141+MuhammadHafizhRaharja@users.noreply.github.com>
2024-10-24 08:21:12 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -3,21 +3,16 @@ import java.util.Scanner;
 public class Nomor1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // Input jam lembur
         int jamLembur = scanner.nextInt();
+        double honor;
         
-        double honor = 0;
         if (jamLembur <= 16) {
             honor = jamLembur * 10000;
         } else {
             honor = (16 * 10000) + ((jamLembur - 16) * 15000);
         }
         
-        // Output
         System.out.printf("Total jam lembur: %d\n", jamLembur);
         System.out.printf("Honor lembur: %.1f\n", honor);
-        
-        scanner.close();
     }
 }
