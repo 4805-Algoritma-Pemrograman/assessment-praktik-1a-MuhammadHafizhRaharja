cc890c80f9e31acdc931ee271b193040e83f5891
Hafizh <110800141+MuhammadHafizhRaharja@users.noreply.github.com>
2024-10-24 07:47:40 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,62 @@
-public class Nomor2{
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Nomor2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int tripBeliau = 0, tripMereka = 0, tripKita = 0;
+        double omzetBeliau = 0, omzetMereka = 0, omzetKita = 0;
+        double biayaPerKm = 13000;
+        String lagi;
+
+        do {
+            System.out.print("Masukan nama driver [Beliau/Mereka/Kita]: ");
+            String driver = input.next();
+
+            System.out.print("Lama trip (km): ");
+            int lamaTrip = input.nextInt();
+
+            switch (driver.toLowerCase()) {
+                case "beliau":
+                    tripBeliau += lamaTrip;
+                    omzetBeliau += lamaTrip * biayaPerKm;
+                    break;
+                case "mereka":
+                    tripMereka += lamaTrip;
+                    omzetMereka += lamaTrip * biayaPerKm;
+                    break;
+                case "kita":
+                    tripKita += lamaTrip;
+                    omzetKita += lamaTrip * biayaPerKm;
+                    break;
+                default:
+                    System.out.println("Nama driver tidak valid!");
+                    break;
+            }
+
+            System.out.print("Input lagi (Y/N)?: ");
+            lagi = input.next();
+
+        } while (lagi.equalsIgnoreCase("Y"));
+
+        int totalTrip = tripBeliau + tripMereka + tripKita;
+
+        String driverTertinggi;
+        double omzetTertinggi;
+        if (omzetBeliau >= omzetMereka && omzetBeliau >= omzetKita) {
+            driverTertinggi = "Beliau";
+            omzetTertinggi = omzetBeliau;
+        } else if (omzetMereka >= omzetBeliau && omzetMereka >= omzetKita) {
+            driverTertinggi = "Mereka";
+            omzetTertinggi = omzetMereka;
+        } else {
+            driverTertinggi = "Kita";
+            omzetTertinggi = omzetKita;
+        }
+
+        System.out.println("Total trip yang dilakukan driver = " + totalTrip);
+        System.out.println("Omzet tertinggi diperoleh oleh " + driverTertinggi + " dengan total Omzet = " + omzetTertinggi);
+
+        input.close();
     }
 }
