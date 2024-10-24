604d8e1b206c6bfb54d1a2ddd25d6e4bdb4dc371
Hafizh <110800141+MuhammadHafizhRaharja@users.noreply.github.com>
2024-10-24 08:07:54 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -4,17 +4,16 @@ public class Nomor1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int jamLembur = scanner.nextInt();
-        double honor = 0;
+        double honor;
 
-        if (jamLembur < 16) {
-            honor = jamLembur * 100000;
-        } else if (jamLembur == 16) {
-            honor = jamLembur * 100000;
-        } else if (jamLembur > 16) {
-            honor = 16 * 100000 + (jamLembur - 16) * 110000;
+        if (jamLembur <= 16) {
+            honor = jamLembur * 100000.00;
+        } else {
+            honor = 16 * 100000.00 + (jamLembur - 16) * 110000.00;
         }
 
-        System.out.printf("Honor lembur: %.2f%n", honor);
+        System.out.printf("%.2f%n", honor);
+
         scanner.close();
     }
 }
